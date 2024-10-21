c824da35841193030de49014aa9938ed05c05bb2
Randhal Ramirez <randhal45@gmail.com>
2024-10-21 16:17:26 -0600

    New solvers activity added.

---
+++ task_solvers.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+#include<math.h>
+
+/* Task proposed on 10/21/2024 
+ * working with 
+ * Bisection method
+ * Newton's method
+ * Secant method
+ * Fixed-Point iteration
+ * Default
+ */
+
+double fun(double a){
+    double gato = pow(6, a)-1;
+    return gato; 
+}
+/*
+void bisection(double a, double b)
+{
+    double ans;
+    double tol = 0.000006;
+
+    if ()
+
+    printf( "Classical bisectcion method solution: %lf", ans );
+}
+
+void ()
+{
+    printf( "Load game called" );
+}
+
+void playmultiplayer()
+{
+    printf( "Play multiplayer game called" );
+}
+
+void playmultiplayer()
+{
+    printf( "Play multiplayer game called" );
+}
+*/
+
+int main()
+{
+    double a = fun(1);
+    printf("function at: %lf", a);
+    /*
+    int input;
+
+    printf( "1. Play game\n" );
+    printf( "2. Load game\n" );
+    printf( "3. Play multiplayer\n" );
+    printf( "4. Exit\n" );
+    printf( "Selection: " );
+    scanf( "%d", &input );
+    switch ( input ) {
+    */
+}
