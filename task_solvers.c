cb499911b49791802d508a2fea7c3ab0df4c22fd
Randhal Ramirez <randhal45@gmail.com>
2024-10-23 17:20:06 -0600

    All solved.

--- task_solvers.c
+++ task_solvers.c
@@ -118,10 +118,10 @@ double secant(double x0, double x1){
 
 
 // Define a secant method function.
-double secant(double x0, double x1){
+double fixpoint(double x0){
 
     double eps = 0.000001; 
-    double x2 = x1 - fun(x1)*((x1-x0)/(fun(x1)-fun(x0)));
+    double x1 = pow(x0, 6)-1;
 
     int q = 0;
 
@@ -129,23 +129,42 @@ double secant(double x0, double x1){
 
 	q+=1;
 
-	if (fabs(fun(x2)) < eps){
+	if (fabs(fun(x1)) < eps){
 	    break;
 	}// End if
 	
 	x0 = x1;
-	x1 = x2;
-	x2 = x2 = x1 - fun(x1)*((x1-x0)/(fun(x1)-fun(x0)));
+	x1 = pow(x0, 6)-1;
 	    
 
     }// End for
 
-    printf("Function x^6-x-1 solved using secant algorithm \n answer = %.8lf in %d steps.\n", x2, q);
+    printf("Function x^6-x-1 solved using Fix-Point algorithm \n answer = %.8lf in %d steps.\n", x1, q);
 
 }// End pointer
 int main()
 {
-    bisection(-1,0);
-    newrap(-0.5);
-    secant(-1,0);
+    int input;
+    printf( "1. Bisection method.\n" );
+    printf( "2. Newton method.\n" );
+    printf( "3. Secant method.\n" );
+    printf( "4. Fix point iteration.\n" );
+    printf( "Selection: " );
+    scanf( "%d", &input );
+    switch ( input ) {
+        case 1:
+	    bisection(-1,0);
+	    break;
+	case 2:
+	    newrap(-0.5);
+	    break;
+	case 3:
+	    secant(-1,0);
+	    break;
+	case 4:
+	    fixpoint(-0.5);
+	    break;
+	default:
+	    newrap(-0.5);}
+
 }
