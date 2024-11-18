897d3c5f8696bef2a37d72dd90b1660365405788
Randhal Ramirez <randhal45@gmail.com>
2024-11-18 15:10:44 -0700

    commit

--- efficiency.c
+++ efficiency.c
@@ -3,32 +3,32 @@
 #include <cblas.h>
 
 
-int main(double argc, char* argv[]) 
+int main(int argc, char* argv[]) 
 {
   clock_t start, end, start1, end1;
-  double cpu_time_used;
+  int cpu_time_used;
      
   start = clock();
      /* Do the work. */
-  double len = 1e11;
-  double x = 1;
-  double y = -1;
-  double t;
-  double ans = 0;
+  int len = 1e6;
+  int x = 1;
+  int y = -1;
+  int t;
+  int ans = 0;
 
-  for(double i; i < len; i++){
+  for(int i; i < len; i++){
     ans = ans + x*y;
     t = x;
     x = y;
     y = t;
-    //prdoublef("x = %d, y = %d dot_product = %d \n", x, y, ans);
+    //printf("x = %d, y = %d dot_product = %d \n", x, y, ans);
   }
 
   end = clock();
 
-  cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-  // prdouble the result
-  printf("This task took %lf seconds to execute \n", cpu_time_used);
+  cpu_time_used = ((int) (end - start)) / CLOCKS_PER_SEC;
+  // print the result
+  printf("This task took %d seconds to execute \n", cpu_time_used);
 
   return 0.;
 }
