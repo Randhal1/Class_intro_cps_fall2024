3b83124fd82843b0373cdaadeba0f59f3aecf261
Randhal Ramirez <randhal45@gmail.com>
2024-11-18 16:18:44 -0700

    new task added

---
+++ num-derivative-approximation/num_derivative.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+
+int main(int argc, char* argv[]) {
+
+	int N = atoi(argv[1]);
+
+	double h = 2*M_PI/N;
+
+	printf("int N = %d \n", N);
+
+} // main func
